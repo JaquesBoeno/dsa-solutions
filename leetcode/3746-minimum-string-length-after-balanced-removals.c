21808af08c44321455fab783f438851bec66cabf
Jaques Boeno <jaquesboeno@proton.me>
2026-04-10 14:23:11 -0300

    [leetcode] Added 3746. Minimum String Length After Balanced Removals Solution

---
+++ leetcode/3746-minimum-string-length-after-balanced-removals.c
@@ -0,0 +1,16 @@
+int minLengthAfterRemovals(char* s) {
+	int d = 0;
+
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (s[i] == 'a') {
+			d++;
+		} else {
+			d--;
+		}
+	}
+
+	if (d < 0)
+		return -d;
+	else
+		return d;
+}
