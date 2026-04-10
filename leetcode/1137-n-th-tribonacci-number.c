c5ae42c7ae41e2fe0b162fc026f975f9e5f218fe
Jaques Boeno <jaquesboeno@proton.me>
2026-04-10 14:22:34 -0300

    [leetcode] Added 1137. N-th Tribonacci Number Solution

---
+++ leetcode/1137-n-th-tribonacci-number.c
@@ -0,0 +1,16 @@
+int tribonacci(int n) {
+	int a = 0, b = 1, c = 1, d = 0;
+	if (n == 0)
+		return 0;
+	if (n < 3)
+		return 1;
+
+	for (; n > 2; n--) {
+		d = a + b + c;
+		a = b;
+		b = c;
+		c = d;
+	}
+
+	return d;
+}
