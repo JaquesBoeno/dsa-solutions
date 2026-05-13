e9712f24c7d98baf770b499e6c42181f9626285e
Jaques Boeno <jaquesboeno@proton.me>
2026-05-13 09:35:19 -0300

    new style format

--- leetcode/0001-two-sum.c
+++ leetcode/0001-two-sum.c
@@ -8,8 +8,8 @@ int *twoSum( int *nums, int numsSize, int target, int *returnSize ) {
 		int k = target - nums[i];
 		for ( int j = i + 1; j < numsSize; j++ ) {
 			if ( nums[j] == k ) {
-				result[0] = i;
-				result[1] = j;
+				result[0]	= i;
+				result[1]	= j;
 				*returnSize = 2;
 				return result;
 			}
